2ff55b9239c55288b0a673ba9017f95b0b4ab70f
iacostap23 <iacostap@eafit.edu.co>
2025-02-05 21:53:48 -0500

    busqueda de peliculas

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
-from movie import views
+
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',movieViews.home),
-    path('about/', views.about )
+    path('about/', movieViews.about )
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
